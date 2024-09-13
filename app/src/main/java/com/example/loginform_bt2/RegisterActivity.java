adb070e952beb20aac8d78d177e984fbd11cfc98
hoangdinhlamhai <hoangdinhlamhai1@gmail.com>
2024-09-13 22:14:03 +0700

    Initial commit

--- app/src/main/java/com/example/loginform_bt2/RegisterActivity.java
+++ app/src/main/java/com/example/loginform_bt2/RegisterActivity.java
@@ -2,7 +2,6 @@ package com.example.loginform_bt2;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,30 +13,30 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
     private EditText usernameInput;
     private EditText passwordInput;
-    private Button loginBtn;
+    private Button registerBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_register);
 
-        usernameInput = findViewById(R.id.username_input);
-        passwordInput = findViewById(R.id.password_input); // Sửa ID theo layout của bạn
-        loginBtn = findViewById(R.id.login_btn); // Sửa ID theo layout của bạn// Sửa ID theo layout của bạn
+        usernameInput = findViewById(R.id.inputUsername);
+        passwordInput = findViewById(R.id.inputPassword); // Sửa ID theo layout của bạn
+        registerBtn = findViewById(R.id.btnRegister); // Sửa ID theo layout của bạn// Sửa ID theo layout của bạn
 
-        loginBtn.setOnClickListener(new View.OnClickListener() {
+        registerBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String username = usernameInput.getText().toString();
                 String password = passwordInput.getText().toString();
 
                 // Giả sử bạn đã định nghĩa các TextView này
-                TextView textViewUsername = findViewById(R.id.textViewUsername);
-                TextView textViewPassword = findViewById(R.id.textViewPassword);
+                TextView textViewUsername = findViewById(R.id.textView3);
+                TextView textViewPassword = findViewById(R.id.textView4);
 
                 // Thiết lập giá trị cho các TextView
                 textViewUsername.setText("Username: " + username);
@@ -45,11 +44,11 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        TextView textViewLogin = findViewById(R.id.register);
+        TextView textViewLogin = findViewById(R.id.alreadyHaveAccount);
         textViewLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
                 startActivity(intent);
             }
         });
